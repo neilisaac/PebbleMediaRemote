40a8a8e6d7076e6860cd3bc9125b506744079358
Neil Isaac <isaac.neil@gmail.com>
2015-10-12 16:06:18 -0400

    Basic server code

--- server.py
+++ server.py
@@ -1,23 +1,126 @@
 #!/usr/bin/env python
 
+import subprocess
+import sys
+import time
 
-def pulse(microseconds):
-    pass
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+IR_PIN = 13
+HTTP_PORT = 5000
+
+
+def set_led(color):
+    print 'calling expled', color
+    subprocess.call(['/usr/bin/expled', color])
+
+
+def fast_gpio(*args):
+    print 'calling fast-gpio', args
+    subprocess.call(['/usr/sbin/fast-gpio'] + list(map(str, args)))
+
+
+def pulse(seconds):
+    fast_gpio('pwm', 37900, IR_PIN)
+    time.sleep(seconds)
+    fast_gpio('set', IR_PIN, 0)
 
 
 def send_nec(code):
-    pass
+    # based on http://techdocs.altium.com/display/FPGA/NEC+Infrared+Transmission+Protocol
+
+    print 'sending', code
+
+    pulse(0.009)  # 9ms leading burst
+    time.sleep(0.0054)  # 4.5ms space
+
+    for bit in '{:020b}'.format(int(code, 16)):
+        if bit == '0':
+            print 'sending 0'
+            pulse(0.0005625)
+            time.sleep(0.0005625)
+        else:
+            print 'sending 1'
+            pulse(0.0005625)
+            time.sleep(0.0016875)
+
+    pulse(0.0005625)  # 562.5us end of transmission
 
 
 def send_samsung(code):
-    pass
+    # based on http://rusticengineering.com/2011/02/09/infrared-room-control-with-samsung-ir-protocol/
+
+    print 'sending', code
+
+    pulse(0.0045)  # 4.5ms on
+    time.sleep(0.0045)  # 4.5ms off
+
+    for bit in '{:020b}'.format(int(code, 16)):
+        if bit == '0':
+            print 'sending 0'
+            pulse(0.000590)
+            time.sleep(0.000590)
+        else:
+            print 'sending 1'
+            pulse(0.000590)
+            time.sleep(0.00169)
+
+    pulse(0.000590)
+    time.sleep(0.000590)
+
+
+def send(command):
+    print 'sending:', command
+    parts = command.split('/')
+    if len(parts) != 2:
+        return 'invalid command: ' + command
+
+    component, cmd = parts
+    if component not in CODES:
+        return 'unknown component: ' + component
+
+    if cmd not in CODES[component]:
+        return 'unknown command: ' + cmd
+
+    code = CODES[component][cmd]
+    CALLBACKS[component](code)
+    return 'OK'
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        body = self.rfile.read()
+        try:
+            command = "/".join(self.path.split("/")[-2:])
+            result = send(command)
+        except Exception as ex:
+            result = str(ex)
+
+        self.send_response(200 if result == 'OK' else 500)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(result)
+        return
 
 
 def main():
-    pass
+    set_led('0x00ff00')
+    fast_gpio('set-output', IR_PIN)
+
+    print 'starting http server on port ' , HTTP_PORT
+    server = HTTPServer(('', HTTP_PORT), Handler)
+    try:
+    	server.serve_forever()
+    except KeyboardInterrupt:
+    	server.socket.close()
+        sys.exit(0)
 
 
-codes = {
+CALLBACKS = dict(tv=send_samsung, preamp=send_nec)
+
+
+CODES = {
     # Samsung UN32EH5300
     'tv': {
         'power':   'E0E040BF',
@@ -37,7 +140,7 @@ codes = {
         'mute':    'E0E0F00F',
         'chup':    'E0E048B7',
         'chdown':  'E0E008F7',
-    }
+    },
 
     # Emotiva XDA-2
     'preamp': {
@@ -54,7 +157,7 @@ codes = {
         'coaxial1': '60FF23DC',
         'coaxial2': '60FFE11E',
         'src':      '60FF639C',
-    }
+    },
 }
 
 
